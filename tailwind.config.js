8d838717aede57d8467563a160a5b2ba89201d53
atmarx <atm26@drexel.edu>
2026-01-05 09:36:44 -0500

    fixed homepage cards

--- tailwind.config.js
+++ tailwind.config.js
@@ -32,6 +32,62 @@ export default {
 					900: '#466346',
 					950: '#233523'
 				},
+				// Warm amber for Symptoms (body awareness)
+				amber: {
+					50: '#fffbf5',
+					100: '#fef3e2',
+					200: '#fde4c4',
+					300: '#f9d19f',
+					400: '#f4b86d',
+					500: '#e9a654',
+					600: '#d4883a',
+					700: '#b06a2e',
+					800: '#8f552b',
+					900: '#754726',
+					950: '#3f2312'
+				},
+				// Soft teal for Considerations (thoughtful, calm)
+				teal: {
+					50: '#f0fdfa',
+					100: '#ccfbf1',
+					200: '#9ef5e4',
+					300: '#6ae8d4',
+					400: '#3dd4be',
+					500: '#22b8a5',
+					600: '#189486',
+					700: '#18766c',
+					800: '#185e57',
+					900: '#184e49',
+					950: '#082f2c'
+				},
+				// Warm coral for Partners (connection, warmth)
+				coral: {
+					50: '#fef5f2',
+					100: '#fee8e2',
+					200: '#fdd5ca',
+					300: '#fbb8a5',
+					400: '#f79072',
+					500: '#ed6d4a',
+					600: '#da5130',
+					700: '#b74025',
+					800: '#973823',
+					900: '#7d3323',
+					950: '#44170e'
+				},
+				// Soft lavender for Finding Care (seeking, navigation)
+				lavender: {
+					50: '#f8f7fc',
+					100: '#f1eef9',
+					200: '#e5e0f4',
+					300: '#d2c8eb',
+					400: '#b9a7de',
+					500: '#a088cf',
+					600: '#8b6abc',
+					700: '#7756a3',
+					800: '#634887',
+					900: '#533d6f',
+					950: '#35254a'
+				},
 				cream: '#FDF8F5',
 				surface: '#FFFFFF',
 				text: {
